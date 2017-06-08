e15a41b8a1b8e29732ac8327cc27c8ea49ceb913
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-06-08 16:01:52 -0400

    Fix new migration so it will run on Django 1.9

--- annotator_store/migrations/0002_annotation_quote_text_optional.py
+++ annotator_store/migrations/0002_annotation_quote_text_optional.py
@@ -8,7 +8,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0008_alter_user_username_max_length'),
         ('annotator_store', '0001_initial'),
     ]
 
